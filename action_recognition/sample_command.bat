9299d1005988ce9b84dbc6db5e4f195258eda6d6
manman4 <manchanr4@gmail.com>
2020-09-15 16:34:26 +0900

    sampleのコマンド追加

---
+++ action_recognition/sample_command.bat
@@ -0,0 +1,1 @@
+python action_recognition.py -m_en models/driver-action-recognition-adas-0002-encoder.xml -m_de models/driver-action-recognition-adas-0002-decoder.xml -i ./inputVideo.mp4 -lb driver_actions.txt
